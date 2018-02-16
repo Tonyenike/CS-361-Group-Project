d47ff9fffed9fa4a9a800ee8ea20b5efc86fe481
tonyenike <martinb3@kec1130-17.engr.oregonstate.edu>
2018-02-15 17:41:37 -0800

    Added more server side stuff

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 /*
  *  Server code.
+ *  
+ *  Required packages:	express
+ *  			express-handlebars
+ *  			handlebars
+ *
+ *  type "npm install [package-name]" to install the package
+ *
+ *  If you pull the entire repository from github, then all the packages should
+ *  already be installed. It is on you, however, to ensure that you have
+ *  npm on whatever computer that you are running on. If you run the code
+ *  on the ENGR server, the npm tool is already installed.
  *
  *  Type: "npm start" to run the server.
- *
+ * 
  */
 
 
@@ -26,15 +37,44 @@ app.get('/', function(req, res) {
     	res.status(200).render('tutorials.handlebars', context);
 });
 
- /*
- *   Use the public folder for styling assets and javascript.
+/*
+ *   Load each of the three UI pages if the user requests it.
  */
 
+
+app.get('/tutorials', function(req, res) {
+	context = {style: "./tutorials.css"};
+    	res.status(200).render('tutorials.handlebars', context);
+});
+
+app.get('/cube', function(req, res) {
+	context = {style: "./cube.css"};
+    	res.status(200).render('cube.handlebars', context);
+});
+
+app.get('/letters', function(req, res) {
+	context = {style: "./letters.css"};
+    	res.status(200).render('letters.handlebars', context);
+});
+
+/*
+*   Use the public folder for styling assets and javascript.
+*/
+
 app.use(express.static('public'));
+
+/*
+*  If the content that the user requests doesn't exist, send a 404 error.
+*/
+
 
 app.get('*', function(req, res) {
 	res.status(404).render('404.handlebars');
 });
+
+/*
+*  Set the server to listen on port [port-number].
+*/  
 
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
